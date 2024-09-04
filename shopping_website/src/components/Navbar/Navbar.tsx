f8974167543abb544e1c08e9f179da17a38b2e9a
Alice <alicezheng888@gmail.com>
2024-09-04 18:45:21 +0200

    feat(#11): create navbar and connect to pages

---
+++ shopping_website/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,36 @@
+import { CgProfile } from "react-icons/cg";
+import { FaRegHeart } from "react-icons/fa";
+import { RiShoppingBag3Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navlogo">
+                <Link to={"/"}>
+                    <h1>RealShop</h1>
+                </Link>
+            </div>
+            <div className="searchfield">
+
+            </div>
+            <div className="navlinks">
+                <Link to={"/ShoppingCart"}>
+                    <RiShoppingBag3Line />
+                    <h3>Shopping Cart</h3>
+                </Link>
+                <Link to={"/Favorits"}>
+                    <FaRegHeart />
+                    <h3>Favorits</h3>
+                </Link>
+                <Link to={"/Profile"}>
+                    <CgProfile />
+                    <h3>Profile</h3>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
