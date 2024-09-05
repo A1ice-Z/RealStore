219b43c9ac503a16277a2841de5b3738df1054b4
tiinajh <tiinajh@ntnu.no>
2024-09-05 12:05:18 +0200

    bug(#3): page empty

--- shopping_website/src/App.tsx
+++ shopping_website/src/App.tsx
@@ -1,9 +1,17 @@
 import Router from './Router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a QueryClient instance for TanStack Query
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router />
+    // Wrap the entire Router with QueryClientProvider
+    <QueryClientProvider client={queryClient}>
+      <Router />
+    </QueryClientProvider>
   );
 }
 
 export default App;
+
--- shopping_website/src/pages/Home.tsx
+++ shopping_website/src/pages/Home.tsx
@@ -1,11 +1,35 @@
 import Navbar from "../components/Navbar/Navbar"
+import {useQuery} from '@tanstack/react-query'
+
+const { isPending, error, data } = useQuery({
+    queryKey: ['products'],
+    queryFn: () =>
+      fetch('https://fakestoreapi.com/products').then((res) =>
+        res.json(),
+      ),
+    })
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <div>
+            {isPending && <p>Loading...</p>}
+            {error && <p>{error.message}</p>}
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))', gap: '20px' }}>
+            {data && data.map((item: any) => (
+                    <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '8px' }}>
+                    <img src={item.image} alt={item.title} style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
+                    <h2 style={{ fontSize: '18px', margin: '10px 0' }}>{item.title}</h2>
+                    <p><strong>Price:</strong> {item.price}</p>
+                    <p><strong>Category:</strong> {item.category}</p>
+                    <p>{item.description}</p>
+                    </div>
+                ))}
+                </div>
+            </div>
         </>
     )
 }
 
-export default Home
+export default Home;
