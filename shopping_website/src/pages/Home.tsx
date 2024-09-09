4c9426b30738d7b7befe6d1a26b88a1e58e01624
tiinajh <tiinajh@ntnu.no>
2024-09-09 19:27:52 +0200

    refactor(#3): extact product fetching and listing into  separate components

--- shopping_website/src/pages/Home.tsx
+++ shopping_website/src/pages/Home.tsx
@@ -1,43 +1,54 @@
 import Navbar from "../components/Navbar/Navbar"
-import { useQuery } from '@tanstack/react-query'
-
-interface Product {
-    id: number;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
-}
+import { useProducts } from "../hooks/useProducts";
 
 const Home = () => {
 
-    const { isLoading, error, data } = useQuery<Product[], Error>({
-        queryKey: ['products'],
-        queryFn: () =>
-            fetch('https://fakestoreapi.com/products').then((res) => res.json()),
-    })
+    const { isLoading, error, data } = useProducts();
 
     return (
         <>
             <Navbar />
             <div>
             {isLoading && <p>Loading...</p>}
-            {error && <p>{(error as Error).message}</p>}
-            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))', gap: '20px' }}>
-            {data && data.map((item) => (
-                    <div key={item.id} style={{ border: '1px solid #ccc', padding: '10px', borderRadius: '8px' }}>
-                    <img src={item.image} alt={item.title} style={{ width: '100%', height: 'auto', borderRadius: '8px' }} />
-                    <h2 style={{ fontSize: '18px', margin: '10px 0' }}>{item.title}</h2>
-                    <p><strong>Price:</strong> {item.price}</p>
-                    <p><strong>Category:</strong> {item.category}</p>
+            {error && <p>{error.message}</p>}
+            <div
+                style={{
+                display: 'grid',
+                gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+                gap: '20px',
+                }}
+            >
+                {data &&
+                data.map((item) => (
+                    <div
+                    key={item.id}
+                    style={{
+                        border: '1px solid #ccc',
+                        padding: '10px',
+                        borderRadius: '8px',
+                    }}
+                    >
+                    <img
+                        src={item.image}
+                        alt={item.title}
+                        style={{ width: '100%', height: 'auto', borderRadius: '8px' }}
+                    />
+                    <h2 style={{ fontSize: '18px', margin: '10px 0' }}>
+                        {item.title}
+                    </h2>
+                    <p>
+                        <strong>Price:</strong> {item.price}
+                    </p>
+                    <p>
+                        <strong>Category:</strong> {item.category}
+                    </p>
                     <p>{item.description}</p>
                     </div>
                 ))}
-                </div>
+            </div>
             </div>
         </>
-    )
+    );
 }
 
 export default Home;
