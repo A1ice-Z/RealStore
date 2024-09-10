ee8a29531a8bbd8cd04f4c144277f346ea798ab7
Alice <alicezheng888@gmail.com>
2024-09-10 09:34:55 +0200

    feat(#18): create popup for hamgurger manu

--- shopping_website/src/components/Navbar/Navbar.tsx
+++ shopping_website/src/components/Navbar/Navbar.tsx
@@ -2,48 +2,73 @@ import { CiSearch, CiHeart } from "react-icons/ci";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import { useState } from "react";
 
 const Navbar = () => {
-    return (
-        <nav className={styles.navbar}>
-            <RxHamburgerMenu className={styles.hamburgmenu} />
+    const [isOpen, setIsOpen] = useState(false);
 
-            <div className={styles.navpage}>
-                <Link to={"/"} className={styles.navlink}>
-                    <p>HOME</p>
-                </Link>
-                <Link to={"/"} className={styles.navlink}>
-                    <p>SHOP</p>
-                </Link>
-                <Link to={"/"} className={styles.navlink}>
-                    <p>ABOUT</p>
-                </Link>
-                <Link to={"/"} className={styles.navlink}>
-                    <p>CONTACT</p>
-                </Link>
-            </div>
-            <div className={styles.navlogo}>
-                <Link to={"/"}>
-                    <img src="/Logo.svg" alt="logo" className={styles.logoimg} />
-                </Link>
-            </div>
-            <div className={styles.navlinks}>
-                <Link to={"/ShoppingCart"} className={styles.cardbutton}>
-                    <p>MY CART</p>
+    return (
+        <main>
+            <nav className={styles.navbar}>
+                {isOpen && <div className={styles.mobilemenu}>
+                    <div className={styles.crosspos}>
+                        <RxCross1 className={styles.cross} onClick={() => setIsOpen(false)} />
+                    </div>
+
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>HOME</p>
+                    </Link>
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>SHOP</p>
+                    </Link>
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>ABOUT</p>
+                    </Link>
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>CONTACT</p>
+                    </Link>
+                    <Link to={"/Favorites"} className={styles.navlink}>
+                        <p>FAVORITES</p>
+                    </Link>
+                </div>}
+                <RxHamburgerMenu className={styles.hamburgmenu} onClick={() => setIsOpen(true)} />
+                <div className={styles.navpage}>
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>HOME</p>
+                    </Link>
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>SHOP</p>
+                    </Link>
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>ABOUT</p>
+                    </Link>
+                    <Link to={"/"} className={styles.navlink}>
+                        <p>CONTACT</p>
+                    </Link>
+                </div>
+                <div className={styles.navlogo}>
+                    <Link to={"/"}>
+                        <img src="/Logo.svg" alt="logo" className={styles.logoimg} />
+                    </Link>
+                </div>
+                <div className={styles.navlinks}>
+                    <Link to={"/ShoppingCart"} className={styles.cardbutton}>
+                        <p>MY CART</p>
+                        <HiOutlineShoppingBag className={styles.whiteicon} />
+                    </Link>
+                    <Link to={"/Favorites"} className={styles.navlink}>
+                        <CiHeart className={styles.blackicon} />
+                    </Link>
+                    <Link to={"/"} className={styles.navlink}>
+                        <CiSearch className={styles.blackicon} />
+                    </Link>
+                </div>
+                <Link to={"/ShoppingCart"} className={styles.shoppingButton}>
                     <HiOutlineShoppingBag className={styles.whiteicon} />
                 </Link>
-                <Link to={"/Favorits"} className={styles.navlink}>
-                    <CiHeart className={styles.blackicon} />
-                </Link>
-                <Link to={"/"} className={styles.navlink}>
-                    <CiSearch className={styles.blackicon} />
-                </Link>
-            </div>
-            <Link to={"/ShoppingCart"} className={styles.shoppingButton}>
-                <HiOutlineShoppingBag className={styles.whiteicon} />
-            </Link>
-        </nav>
+            </nav>
+        </main>
     )
 }
 
