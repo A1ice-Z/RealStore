3bb438144b09238333209d672ee7fd3b3bc1ef11
tiinajh <tiinajh@ntnu.no>
2024-09-10 12:23:47 +0200

    refactor(#3): removed unnecessary comments

--- shopping_website/src/App.tsx
+++ shopping_website/src/App.tsx
@@ -1,12 +1,10 @@
 import Router from './Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// Create a QueryClient instance for TanStack Query
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    // Wrap the entire Router with QueryClientProvider
     <QueryClientProvider client={queryClient}>
       <Router />
     </QueryClientProvider>
