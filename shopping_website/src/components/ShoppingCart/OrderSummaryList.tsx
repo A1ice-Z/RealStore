3a63396e36382d3d2d2e36d52eafff79c42de6cf
Alice <alicezheng888@gmail.com>
2024-09-14 16:25:27 +0200

    fix: change the bugs that came with trying to change div

--- shopping_website/src/components/ShoppingCart/OrderSummaryList.tsx
+++ shopping_website/src/components/ShoppingCart/OrderSummaryList.tsx
@@ -23,21 +23,21 @@ const OrderSummaryList = () => {
             <section className={styles.box}>
                 <h2 className={styles.title}>ORDER SUMMARY</h2>
                 <div className={styles.textbox}>
-                    <span className={styles.text}>Subtotal</span>
-                    <span className={styles.text}>$ 100.00</span>
+                    <p className={styles.text}>Subtotal</p>
+                    <p className={styles.text}>$ 100.00</p>
                 </div>
                 <div className={styles.textbox}>
-                    <span className={styles.text}>Shipping</span>
-                    <span className={styles.text}>Free</span>
+                    <p className={styles.text}>Shipping</p>
+                    <p className={styles.text}>Free</p>
                 </div>
                 <div className={styles.totalbox}>
-                    <span className={styles.totaltext}>TOTAL</span>
-                    <span className={styles.taxtext}>(TAX INCL.)</span>
-                    <span className={styles.totaltext}>$ 100.00</span>
+                    <p className={styles.totaltext}>TOTAL</p>
+                    <p className={styles.taxtext}>(TAX INCL.)</p>
+                    <p className={styles.totaltext}>$ 100.00</p>
                 </div>
                 <label className={styles.checkbox}>
                     <input type="checkbox" onChange={e => handleChange(e)} />
-                    <span className={styles.checkboxtext}>I agree to the Terms and Conditions</span>
+                    <p className={styles.checkboxtext}>I agree to the Terms and Conditions</p>
                 </label>
                 <button type="button" disabled={!clicked} className={styles.button} onClick={() => handleClick()}>PAY</button>
             </section>
