daba9cda0905482ebc33eff5e31f2d9b4ec82a90
Alice <alicezheng888@gmail.com>
2024-09-14 15:21:48 +0200

    feat(#7): create home page and make it scalable for mobile version

--- shopping_website/src/pages/Home.tsx
+++ shopping_website/src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import Navbar from "../components/Navbar/Navbar"
 import Footer from "../components/Footer/Footer"
 import styles from "../styles/Home.module.css"
+import { FaArrowRight } from "react-icons/fa6";
+import { Link } from "react-router-dom";
+
 
 const Home = () => {
 
@@ -8,7 +11,16 @@ const Home = () => {
         <>
             <Navbar />
             <main className={styles.homepage}>
-
+                <div className={styles.homebox}>
+                    <div className={styles.informationbox}>
+                        <p className={styles.descriptiontext}>Real Materials</p>
+                        <p className={styles.descriptiontext}>Real Style</p>
+                        <p className={styles.shoptext}>RealStore</p>
+                        <p className={styles.informationtext}>Experience premium clothing, jewelery and electronics made from authentic materials. Choose us for timeless fashion and exceptional craftsmanship</p>
+                        <Link className={styles.link} to={"/Shopping"}><button className={styles.shoppingbutton} onClick={() => "/Shopping"}>Explore <FaArrowRight className={styles.arrow} /> </button></Link>
+                    </div>
+                    <img src="/woman.svg" alt="woman" className={styles.image} />
+                </div>
             </main>
             <Footer />
         </>
