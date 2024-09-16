4ec9d8d5cdee93db13a04ed1bf703edd25339871
Alice <alicezheng888@gmail.com>
2024-09-17 01:33:49 +0200

    feat(#2): create favorites page and make it mobile friendly

--- shopping_website/src/pages/Favorites.tsx
+++ shopping_website/src/pages/Favorites.tsx
@@ -1,10 +1,70 @@
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+import styles from "../styles/Favorites.module.css"
+import { Link } from "react-router-dom";
 
 const Favorites = () => {
     return (
-        <div>
+        <>
             <Navbar />
-        </div>
+            <main className={styles.favoritepage}>
+                <div className={styles.favoritecontainer}>
+                    <nav className={styles.path}>
+                        <Link className={styles.pathlink} to={"/"}>Home</Link> / Favorites
+                    </nav>
+                    <p className={styles.title}>FAVORITES</p>
+                    <section className={styles.favoriteitems}>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+                        <p>hellu</p>
+
+                    </section>
+                </div>
+            </main>
+            <Footer />
+        </>
     );
 }
 
