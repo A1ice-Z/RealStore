ed59b8cf0ded34d5f712f05ad6e221fcee7720f5
tiinajh <tiinajh@ntnu.no>
2024-09-19 15:57:06 +0200

    feat(#30): add test setup and first test

---
+++ shopping_website/src/App.test.tsx
@@ -0,0 +1,15 @@
+import { screen, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
+
+describe("App tests", () => {
+  it("should render the title", () => {
+    render(<App />);
+
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+      })
+    ).toHaveTextContent("RealStore");
+  });
+});
