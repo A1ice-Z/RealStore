53936b28fe7a50f293cb93ffbbf94711d6010ca1
Alice <alicezheng888@gmail.com>
2024-09-20 14:55:58 +0200

    refactor(#43): change the footer

--- shopping_website/src/components/Footer/Footer.tsx
+++ shopping_website/src/components/Footer/Footer.tsx
@@ -10,31 +10,14 @@ const Footer = () => {
                 <img src={footer} alt="Decorative footer image" className={styles.image} />
                 <p className={styles.title} aria-label="Explore Our Catalog Title">EXPLORE  OUR  CATALOG</p>
                 <div className={styles.box} role="region" aria-label="Footer Navigation">
-                    <div className={styles.informationbox}>
-                        <div>
-                            <p className={styles.informationtitle}  aria-label="General Section">GENERAL</p>
-                            <Link to={"/"} className={styles.informationlink}>
-                                <p className={styles.informationtext}  aria-label="Home Link">Home</p>
-                            </Link>
-                            <Link to={"/Shopping"} className={styles.informationlink}>
-                                <p className={styles.informationtext} aria-label="Shop Link">Shop</p>
-                            </Link>
-                        </div>
-                        <div>
-                            <p className={styles.informationtitle} aria-label="Products Section">PRODUCTS</p>
-                            <Link to={"/"} className={styles.informationlink}>
-                                <p className={styles.informationtext} aria-label="Electronics Link">Electronics</p>
-                            </Link>
-                            <Link to={"/"} className={styles.informationlink}>
-                                <p className={styles.informationtext} aria-label="Women's Fashion Link">Women's Fashion</p>
-                            </Link>
-                            <Link to={"/"} className={styles.informationlink}>
-                                <p className={styles.informationtext} aria-label="Men's Fashion Link">Men's Fashion</p>
-                            </Link>
-                            <Link to={"/"} className={styles.informationlink}>
-                                <p className={styles.informationtext} aria-label="Jewelry Link">Jewelry</p>
-                            </Link>
-                        </div>
+                    <div className={styles.informationbox}>                        
+                        <p className={styles.informationtitle}  aria-label="General Section">GENERAL</p>
+                        <Link to={"/"} className={styles.informationlink}>
+                            <p className={styles.informationtext}  aria-label="Home Link">Home</p>
+                        </Link>
+                        <Link to={"/Shopping"} className={styles.informationlink}>
+                            <p className={styles.informationtext} aria-label="Shop Link">Shop</p>
+                        </Link>
                     </div>
                     <div className={styles.divider} role="separator" aria-hidden="true"/>
                     <div className={styles.logobox} aria-label="Footer Logo and Information">
